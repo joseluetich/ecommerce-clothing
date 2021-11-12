baf4ed4d0ce362b166844f80a71e56aa9be1dfe8
mluetich <mluetich@everis.com>
2021-11-12 15:16:00 -0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/home/home";
 import Shop from "./pages/shop/shop";
+import Header from "./components/header/header";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
-      <Route path="/shop" exact>
-        <Shop />
-      </Route>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/shop" exact>
+          <Shop />
+        </Route>
+      </Switch>
     </div>
   );
 }
